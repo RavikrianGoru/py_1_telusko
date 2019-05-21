52c843057e12036849c367c3089e2a7eb75ebc1d
RavikiranGoru <ravikirangoru@gmail.com>
2019-05-22 01:26:10 +0530

    palindrome test case insensitive and ignore punctuations

---
+++ byte_of_python/py_tasks/palindrome.py
@@ -0,0 +1,32 @@
+def main():
+    text = input("Enter text to check palindrome or not:")
+    print(f'Input : {text}')
+
+    if is_palindrome_v2(text):
+        print('Yes, {text} is palindrome.')
+    else:
+        print('NO, {text} is not palindrome.')
+
+
+def is_palindrome(text):
+    return text == text[::-1]
+
+
+def is_palindrome_v2(text):
+    ''' case insensitive ignore punctuation '''
+    forbidden = (' ', '.', ',', ';', ':', '?')
+    text = removePunctuations(text, forbidden)
+    return text == text[::-1]
+
+
+def removePunctuations(text, forbidden):
+    for each in forbidden:
+        text = text.replace(each,'')
+    text = text.lower()
+    return text
+
+
+if __name__ == '__main__':
+    main()
+
+
